af367dad5944bc5b1569940241acf1247f15974e
Shujaat Khan <148489139+shujaatkhanjadoon@users.noreply.github.com>
2024-01-15 08:05:41 +0000

    commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
-    <p className='font-bold text-green-500'>
-      Next Auth
-    </p>
+    <Button size="lg" variant="outline">
+      Click Me
+    </Button>
   )
 }
